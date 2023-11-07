caab2bfbcd8ff636b51a2b338e1a8816dd5e0d31
spencertlogan <100391698+spencertlogan@users.noreply.github.com>
2023-11-07 14:18:48 -0600

    Create Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,34 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ManagerPOS from './ManagerPOS/ManagerPOS';
+import { NavLink, Routes, Route } from 'react-router-dom';
 import './App.css'; // Assuming your CSS is in this file
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <ManagerPOS />
+        <LandingPage />
       </div>
     </Router>
+  );
+}
+
+const LandingPage = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/manager" className={({ isActive }) => isActive ? 'active' : ''}>Manager</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/manager" element={<ManagerPOS />} />
+      </Routes>
+    </div>
   );
 }
 
