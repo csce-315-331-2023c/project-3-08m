b617ba6be4ad57945ce3ffe288ce3591e890bf3b
Affixx7 <arnavsood369@gmail.com>
2023-11-23 22:53:05 -0600

    edit ability for Menu Table

--- client/src/ManagerPOS/components/Tables/MenuTable.js
+++ client/src/ManagerPOS/components/Tables/MenuTable.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridCellEditStopReasons } from '@mui/x-data-grid';
 
 // const serverURL = 'https://project-3-server-ljp9.onrender.com' || 'http://localhost:9000';
 const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:9000';
@@ -57,13 +57,54 @@ const MenuTable = () => {
   if (error) {
     return <p>Error loading menu items: {error}</p>;
   }
+  
+  const handleEdit = (params, event) => {
+    if (params.reason === GridCellEditStopReasons.cellFocusOut) {
+      event.defaultMuiPrevented = true;
+      return;
+    }
+    console.log(params);
+    console.log(event.target.value);
+    // console.log(event);
+    const updateMenu = async (type, updateVals) => {
+      var success = false;
+      try {
+        var response = await fetch(serverURL+"/updateMenu", {
+          method: 'POST',
+          headers: {
+            "Content-type": "application/json; charset = UTF-8"
+          },
+          body: JSON.stringify({[type]: updateVals})
+        });
+        var res = await response.json();
+        success = res.updateSuccess;
+      }
+      catch (error) {
+        console.log(error);
+      }
+      console.log(success);
+      return success;
+    }
+    if (params.field == 'name') {
+      updateMenu('name', {'id': params.row.id, 'name': event.target.value});
+    }
+    else if (params.field == 'price') {
+      updateMenu('price', {'id': params.row.id, 'price': event.target.value});
+    }
+  }
 
   return (
     <Box sx={{ width: '100%', '& .super-app-theme--header': {
       backgroundColor: '#2E4647', color: 'white', fontWeight: 'bold'},}}>
       <DataGrid
+        onCellEditStop={handleEdit}
         rows={menuItems}
         columns={columns}
+        initialState={{
+          sorting: {
+            sortModel: [{field: 'id', sort: 'asc'}]
+          }
+        }}
       />
     </Box>
   );
