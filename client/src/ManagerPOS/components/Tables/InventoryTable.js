c61309287a5be7655f89abc85df5437c5a2a5823
Affixx7 <arnavsood369@gmail.com>
2023-11-23 22:51:19 -0600

    edit ability for Inventory Table

--- client/src/ManagerPOS/components/Tables/InventoryTable.js
+++ client/src/ManagerPOS/components/Tables/InventoryTable.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridCellEditStopReasons } from '@mui/x-data-grid';
 
 // const serverURL = 'https://project-3-server-ljp9.onrender.com';
 const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:9000';
@@ -21,7 +21,7 @@ const columns = [
     headerClassName: 'super-app-theme--header',
     flex: 2,
     minWidth: 150,
-    editable: true,
+    editable: false,
   },
   {
     field: 'amount_remaining',
@@ -54,6 +54,11 @@ const InventoryTable = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const formatDateTime = (dateTime) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric'};
+    return new Date(dateTime).toLocaleDateString(undefined, options);
+  }
+
   useEffect(() => {
     const fetchInventory = async () => {
       try {
@@ -62,6 +67,9 @@ const InventoryTable = () => {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
+        for (let i = 0; i < data.inventory.length; i++) {
+          data.inventory[i].last_restock_date = formatDateTime(data.inventory[i].last_restock_date);
+        }
         setInventory(data.inventory);
       } catch (error) {
         setError(error.message);
@@ -81,12 +89,58 @@ const InventoryTable = () => {
     return <div>Error loading inventory: {error}</div>;
   }
 
+  const handleEdit = (params, event) => {
+    if (params.reason === GridCellEditStopReasons.cellFocusOut) {
+      event.defaultMuiPrevented = true;
+      return;
+    }
+    console.log(params);
+    console.log(event.target.value);
+    const updateInventory = async (type, updateVals) => {
+      var success = false;
+      try {
+        var response = await fetch(serverURL+"/updateInventory", {
+          method: 'POST',
+          headers: {
+            "Content-type": "application/json; charset = UTF-8"
+          },
+          body: JSON.stringify({[type]: updateVals})
+        });
+        var res = await response.json();
+        success = res.updateSuccess;
+      }
+      catch (error) {
+        console.log(error);
+      }
+      console.log(success);
+      return success;
+    }
+    if (params.field == 'name') {
+      updateInventory('name', {'id': params.row.id, 'name': event.target.value});
+    }
+    else if (params.field == 'amount_remaining') {
+      updateInventory('amount_remaining', {'id': params.row.id, 'amountRemaining': event.target.value});
+    }
+    else if (params.field == 'amount_used') {
+      updateInventory('amount_remaining', {'id': params.row.id, 'amountUsed': event.target.value});
+    }
+    else if (params.field == 'min_amount') {
+      updateInventory('minimum_amount', {'id': params.row.id, 'minimumAmount': event.target.value});
+    }
+  }
+
   return (
     <Box sx={{ width: '100%', '& .super-app-theme--header': {
       backgroundColor: '#2E4647', color: 'white', fontWeight: 'bold'},}}>
       <DataGrid
+        onCellEditStop={handleEdit}
         rows={inventory}
         columns={columns}
+        initialState={{
+          sorting: {
+            sortModel: [{field: 'id', sort: 'asc'}]
+          }
+        }}
       />
     </Box>
   );
