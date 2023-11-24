9f6b25e70a58dba36572b0c48a1ef27d88ccfa20
Affixx7 <arnavsood369@gmail.com>
2023-11-23 22:56:10 -0600

    changed data format in Orders Table

--- client/src/ManagerPOS/components/Tables/OrdersTable.js
+++ client/src/ManagerPOS/components/Tables/OrdersTable.js
@@ -14,7 +14,7 @@ const columns = [
     headerClassName: 'super-app-theme--header',
     flex: 1,
     minWidth: 50,
-    editable: true,
+    editable: false,
   },
   {
     field: 'date_time',
@@ -22,7 +22,7 @@ const columns = [
     headerClassName: 'super-app-theme--header',
     flex: 1,
     minWidth: 50,
-    editable: true,
+    editable: false,
   }
 ];
 
@@ -55,8 +55,8 @@ const OrdersTable = () => {
   }, []);
 
   const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return new Date(dateString).toLocaleString(undefined, options);
   };
 
   if (loading) {
@@ -73,6 +73,11 @@ const OrdersTable = () => {
       <DataGrid
         rows={orders}
         columns={columns}
+        initialState={{
+          sorting: {
+            sortModel: [{field: 'id', sort: 'desc'}]
+          }
+        }}
       />
     </Box>
   );
