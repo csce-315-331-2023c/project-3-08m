913481f9eaed1ba8ede129d54646c420c5f10d4d
jeff-ooi <jeffooi@tamu.edu>
2023-11-27 18:26:48 -0600

    weather display skeleton

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -1,4 +1,8 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+
+import { Box, Divider, Icon, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { WbSunny as SunnyIcon, Cloud as CloudyIcon, Thermostat as TemperatureIcon } from '@mui/icons-material';
 
 // getting date
 const date = new Date();
@@ -8,7 +12,7 @@ var year = date.getFullYear();
 var today = `${year}-${month}-${day}`;
 
 const API_KEY = "AJ9C85JYEPDC8Q6S8VDNKMA64";
-const API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/77840/" + today + "?key=" + API_KEY;
+const API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/77840/" + today + "?key=" + API_KEY + "&iconSet=icons2";
 
 function processWeatherData(response) {
     var day = response.days[0];
@@ -16,19 +20,62 @@ function processWeatherData(response) {
     var max = day.tempmax;
     var curr = day.temp;
     var conditions = day.conditions;
-    return [min, max, curr, conditions];
+    var icon = day.icon;
+    return {min, max, curr, conditions, icon};
 }
 
-(async () => {
-    try {
+// const getWeatherData = () => {
+//     const 
+//     var weatherReport = [];
+//     try {
+//         const response = await fetch(API_URL);
+//         const json = await response.json();
+//         weatherReport = processWeatherData(json);
 
-        const response = await fetch(API_URL);
-        const json = await response.json();
-        processWeatherData(json);
+//     } catch (error) {
+//         console.log(error);
+//     }
+//     return weatherReport;
+// }
 
-    } catch (error) {
-        console.log(error);
+export const Weather = () => {
+    const [ weather, setWeather ] = useState({});
+
+    useEffect(() => {
+        const getWeatherData = async () => {
+            try {
+                const response = await fetch(API_URL);
+                const json = await response.json();
+                // console.log(json);
+                setWeather(processWeatherData(json));
+            }
+            catch (error) {
+                console.log(error);
+            }
+            // console.log(weather);
+        }
+        getWeatherData();
+    }, []);
+
+    if (Object.entries(weather).length === 0) {
+        return <div></div>
     }
-  })();
+    // console.log(weather);
 
-  
+    return (
+        // <Box>
+            // <SunnyIcon />
+        // </Box>
+        <>
+            <Icon style={{maxHeight: 70}}>
+                <SunnyIcon />
+                {/* <img src={'./WeatherIcons/'+weather.icon+'.png'}></img> */}
+            </Icon>
+            {` ${weather.min} / ${weather.max} F`}
+            {/* <Typography>
+                Hello
+            </Typography> */}
+        </>
+    )
+}
+
