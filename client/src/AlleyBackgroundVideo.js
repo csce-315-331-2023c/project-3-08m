47ef8fa1bae7a3b2e3cda5d92bc8988a7910411c
Affixx7 <arnavsood369@gmail.com>
2023-11-30 02:45:29 -0600

    added the overlay logo to the login page

--- client/src/AlleyBackgroundVideo.js
+++ client/src/AlleyBackgroundVideo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box} from '@mui/material';
+import logoOverlay from './alley_overlay_logo.svg';
 
 const AlleyBackgroundVideo = () => {
 
@@ -8,24 +9,11 @@ const AlleyBackgroundVideo = () => {
       overflow: 'hidden', 
       position: 'relative', 
       width: '100%', 
-      // Set a padding-top for aspect ratio (e.g., 56.25% for 16:9 aspect ratio)
       paddingTop: '56.25%', 
-      // Add a negative margin to push the video up and cut off the top
       marginTop: '-15%', 
-      // Add a negative margin to push the video down and cut off the bottom
       marginBottom: '-20%',
-      "&:after": { // Create an overlaying pseudo-element
-        content: '""',
-        display: 'block',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        // Adjust the zIndex to be below the content but above the video
-        zIndex: -1,
-      }
     }}>
+      {/* YouTube iframe */}
       <iframe
         src="https://www.youtube.com/embed/2pPCHckv9P8?autoplay=1&controls=0&loop=1&playlist=2pPCHckv9P8&mute=1&modestbranding=1&playsinline=1&rel=0&showinfo=0"
         frameBorder="0"
@@ -34,14 +22,35 @@ const AlleyBackgroundVideo = () => {
         title="YouTube Video"
         style={{
           position: 'absolute',
-          top: '50%', // Center vertically
-          left: '50%', // Center horizontally
-          width: '100vw', // Cover the full viewport width
-          height: 'calc(100vw * (9 / 16))', // Maintain a 16:9 aspect ratio
-          transform: 'translate(-50%, -50%) scale(1.2)', // Adjust scale as needed
-          zIndex: -2, // Ensure the video is behind the overlay
+          top: '50%', 
+          left: '50%', 
+          width: '100vw', 
+          height: 'calc(100vw * (9 / 16))', 
+          transform: 'translate(-50%, -50%) scale(1.2)', 
+          zIndex: -2,
         }}
       />
+
+      {/* Image Overlay */}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%', 
+          left: '50%', 
+          width: '20vw', 
+          height: 'calc(30vw * (9 / 16))', 
+          // width: 300,
+          // height: 300,
+          transform: 'translate(-50%, -50%) scale(1.2)', 
+          backgroundImage: `url(${logoOverlay})`, // Replace with your image path
+          backgroundSize: 'cover',
+          // backgroundPosition: 'center',
+          // opacity: 0.5, // Adjust the opacity as needed
+          zIndex: -1, // Overlay is above the video but below other content
+        }}
+      />
+
+      {/* Any other content you want to display on top */}
     </Box>
   );
 };
